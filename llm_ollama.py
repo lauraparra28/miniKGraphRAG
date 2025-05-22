3566831491d514e27ff73c767d46b63b60fc957d
lauraparra28 <laura.parra28@gmail.com>
2025-05-22 12:15:42 -0300

    update llama3 and queries

--- llm_ollama.py
+++ llm_ollama.py
@@ -3,7 +3,7 @@ from langchain_ollama import OllamaLLM
 def load_llm(model_name="llama3:8b"): # MI PC: deepseek-r1:1.5b  qwen2.5:1.5b gemma3:1b
     return OllamaLLM(
         model=model_name,
-        temperature=0.1,
+        temperature=0.0,
         max_tokens=128,
         base_url="http://127.0.0.1:11434" # cat /etc/resolv.conf | grep nameserver 10.255.255.254 192.168.134.59
     )
--- utils/base_utils.py
+++ utils/base_utils.py
@@ -8,7 +8,8 @@ def read_file(ruta_archivo):
 def load_prompts():
     prompts = {}
     prompts_dir_MY_PC = '/home/laura/ICA/Projetos 2024/miniKGraphRAG/prompts'
-    prompts_dir = '/home/lauraparra85/ICA/miniKGraphRAG/prompts'
+    prompts_dir_PC_ICA = '/home/lauraparra85/ICA/miniKGraphRAG/prompts'
+    prompts_dir = '/home/diego/laura/ICA/miniKGraphRAG/prompts'
     for filename in os.listdir(prompts_dir):
         if filename.endswith('.txt'):
             with open(os.path.join(prompts_dir, filename), 'r', encoding='utf-8') as file:
@@ -17,7 +18,9 @@ def load_prompts():
 
 def load_dataset():
     datasets = {}
-    dataset_dir = '/home/lauraparra85/ICA/miniKGraphRAG/KG data/'
+    dataset_dir_PC_ICA = '/home/lauraparra85/ICA/miniKGraphRAG/KG data/'
+    dataset_dir = '/home/diego/laura/ICA/miniKGraphRAG/KG data/'
+    
     for filename in os.listdir(dataset_dir):
         if filename.endswith('.json'):
             with open(os.path.join(dataset_dir, filename), 'r', encoding='utf-8') as file:
